ca5c2919b7f0de289434429955b04636b6f274d2
Thiago Gibran <gibranlog@gmail.com>
2025-05-10 20:50:39 -0300

    🔥 Reindexação forçada de urban_routes_project com conteúdo atualizado da Sprint 8

---
+++ urban_routes_project/data.py
@@ -0,0 +1,9 @@
+URBAN_ROUTES_URL = 'https://cnt-8daedaad-7f43-4b3a-a87e-cd2aa059d925.containerhub.tripleten-services.com?lng=pt'
+# Será preenchido depois com o link do servidor
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '123'
+MESSAGE_FOR_DRIVER = 'Traga os Lanches'
